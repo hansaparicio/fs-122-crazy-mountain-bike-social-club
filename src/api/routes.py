d5657a71e81accc3f452e343084254c0410374b5
Hans Aparicio <hansaparicio@gmail.com>
2026-01-15 11:34:44 +0100

    merge initial changes

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from api.utils import generate_sitemap, APIException, is_valid_email
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
