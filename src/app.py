cad6b8bbe407ee9e50c3b61e7c778e87fc34bf14
Asiiz127 <imasiiz127@gmail.com>
2026-01-15 14:41:51 +0100

    cambios en app.py para ejecutar la bd

--- src/app.py
+++ src/app.py
@@ -26,7 +26,7 @@ if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
         "postgres://", "postgresql://")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
