bcdb892525b7c009f15e5a8b07ca39a2e93e6224
kalambriento <raulreyes2@gmail.com>
2026-02-01 19:01:39 +0100

    Solución carga header en login

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,30 +1,51 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import { Loader } from "../components/Loader/Loader";
 import { useLoader } from "../context/loaderContext";
 import "../styles/footer.css";
-import AiChatDialog from "../components/AiChatDialog";
 import MainHeader from "../components/Header/MainHeader";
-import { use } from "react";
+import { useEffect, useState } from "react";
 
 export const Layout = () => {
   const { isLoading } = useLoader();
   const location = useLocation();
+  const navigate = useNavigate();
+
+  const [authLoading, setAuthLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  setUser(storedUser);
+
+  if (storedUser && location.pathname === "/") {
+    navigate("/home");
+  }
+
+  setAuthLoading(false);
+}, [location.pathname]);
+
+  if (authLoading) return null;
 
   const hideHeader =
     location.pathname === "/login" ||
     location.pathname === "/signup";
+
+  const hideFooter = location.pathname === "/route-registration";
 
   return (
     <>
       {isLoading && <Loader />}
 
       <div className={`app-root ${isLoading ? "is-loading" : ""}`}>
-        {!hideHeader && <MainHeader />}
+        {user && !hideHeader && <MainHeader />}
 
         <Outlet />
-        <Footer />
+
+        {!hideFooter && <Footer />}
       </div>
     </>
   );
 };
+
