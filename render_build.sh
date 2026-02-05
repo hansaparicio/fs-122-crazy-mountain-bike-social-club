55161aa2dd20124ba799c88c29d15d010ddba821
Hans Aparicio <hansaparicio@gmail.com>
2026-02-05 16:55:47 +0100

    update render

--- render_build.sh
+++ render_build.sh
@@ -11,5 +11,6 @@ pip install pipenv
 pipenv install --dev --deploy --ignore-pipfile
 psql "$DATABASE_URL" -c "DROP SCHEMA public CASCADE;"
 psql "$DATABASE_URL" -c "CREATE SCHEMA public;"
+pipenv run flask db stamp head
 pipenv run migrate
 pipenv run upgrade
